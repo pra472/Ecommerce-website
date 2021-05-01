cbbac7c64137e4d71ad7e5c054e31e918bdf4fbf
prameela <83271931+pra472@users.noreply.github.com>
2021-05-01 15:53:24 +0530

    initial commit

---
+++ register.js
@@ -0,0 +1,31 @@
+function validate() {
+	var username=document.getElementById("uname");
+	
+	var password=document.getElementById("pass");
+
+	if(username.value==""||password.value==""){
+		alert("no balnk values allowed");
+		return  false;
+	}
+	else
+	{
+		true;
+	}
+var  number=document.getElementById("ph").value;
+if(res.length==0)
+{
+	document.getElementById("msg").innerHtml="phone number field is empty";
+	return false;
+}
+var email=document.getElementById('email').value;
+
+if(email==0)
+{
+	document.getElementById("msg").innerHtml="email field is empty";
+	return false;
+}
+
+
+	{
+
+	}
